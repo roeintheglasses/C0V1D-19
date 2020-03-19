fac67a14b6813ab6f40dc7352357b56612bc3a29
Hrishi Jangir <hrkjangir@gmail.com>
2020-03-19 16:47:29 +0530

    fixes server errors

--- src/app.js
+++ src/app.js
@@ -1,34 +1,16 @@
 const path = require('path')
 const express = require('express')
-const fs = require('fs');
-const https = require('https');
-// var forceSsl = require('force-ssl-heroku');
-
-
-const privateKey = fs.readFileSync(__dirname + '/sslforfree/private.key');
-const certificate = fs.readFileSync(__dirname + '/sslforfree/certificate.crt');
-const ca = fs.readFileSync(__dirname + '/sslforfree/ca_bundle.crt');
-
-const options = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
+const forceSsl = require('force-ssl-heroku');
 
 const app = express()
-const port = 8085
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../dist')
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath, {
-    dotfiles: 'allow'
-}))
-// app.use(forceSsl);
-
-var server = https.createServer(options, app);
-
-server.listen(port, () => {
+app.use(express.static(publicDirectoryPath))
+app.use(forceSsl);
+app.listen(port, () => {
     console.log("server starting on port : " + port)
 });
