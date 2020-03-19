93b4c71f04a0665dfdf57a1d5e64ed02ad5240df
Hrishi Jangir <hrkjangir@gmail.com>
2020-03-19 15:10:09 +0530

    added force-ssl to force ssl on heroku

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const express = require('express')
+var forceSsl = require('force-ssl-heroku');
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,6 +11,8 @@ const publicDirectoryPath = path.join(__dirname, '../dist')
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+app.use(forceSsl);
+
 
 app.listen(port, () => {
     console.log('Server is up on port : ' + port + '.')
